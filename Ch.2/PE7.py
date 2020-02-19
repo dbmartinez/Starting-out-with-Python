667b7692d207b6554bc7115e3c2336036aa5173c
Daniel Martinez <57377837+dbmartinez@users.noreply.github.com>
2020-02-19 14:35:10 -0800

    Add files via upload

---
+++ Ch.2/PE7.py
@@ -0,0 +1,17 @@
+# Chapter 2
+# Program 7
+# Algorithm
+# 1. Ask user for the miles driven
+# 2. Ask user the amount of gas used
+# 3. Calculate MPG
+# 4. Display result
+
+# User input
+miles_driven = int(input("\nEnter in miles driven: "))
+gallons_used = int(input("Enter in gallons used: "))
+
+# Calculation
+MPG = miles_driven / gallons_used
+
+# Display result
+print("\nMPG: ", MPG)
