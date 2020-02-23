e4148bd65611fbb0d08edce43d94457c952a3086
Daniel Martinez <57377837+dbmartinez@users.noreply.github.com>
2020-02-23 00:16:44 -0800

    Add files via upload

---
+++ Ch.2/PE9.py
@@ -0,0 +1,16 @@
+# Chapter 2
+# Program 9
+# Algorithm
+# 1. Ask user to enter temperature
+# 2. Convert celsius to fahrenheit
+# 3. Display results
+
+# Ask user input
+print("\nThis program will convert celsius to fahrenheit")
+celsius = float(input("Enter temperature in celsius: "))
+
+# Calculation
+fahrenheit = (9 / 5) * celsius + 32
+
+# Display results
+print("Fahrenheit is ", fahrenheit, " degrees")
